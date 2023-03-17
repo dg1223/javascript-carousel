c0dfaca0593453d15806937bf7612ce6b26ff82a
dg1223 <alavi1223@hotmail.com>
2023-03-17 12:02:00 -0400

    set interval for images

---
+++ js/slider.js
@@ -0,0 +1,18 @@
+const images = [
+  "./images/pic-1.jpg",
+  "./images/pic-2.jpg",
+  "./images/pic-3.jpg",
+  "./images/pic-4.jpg",
+  "./images/pic-5.jpg",
+  "./images/pic-6.jpg",
+  "./images/pic-7.jpg",
+  "./images/pic-8.jpg",
+  "./images/pic-9.jpg",
+];
+
+let imgIndex = 0;
+
+setInterval(() => {
+  console.log(imgIndex);
+  imgIndex++;
+}, 1000);
